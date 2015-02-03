38e1f916eacc990e11f2daf191334da3aa96640a
Sam Jacob <samjacob.vethanayagam@cognizant.com>
2015-02-03 17:30:02 +0530

    commiting style changes for CI pages

--- csimcw/application/views/_login_form.php
+++ csimcw/application/views/_login_form.php
@@ -1,27 +1,30 @@
-<div>
+<div class="form2">
 <?php echo form_open(base_url().'index.php/login/')?>
-<div>Login to your account</div>
+<div class="formtitle">Login to your account</div>
+<div class="error">
 <?php echo validation_errors(); ?>
-<span><b><?php echo $login_failed; ?></b></span>
+</div>
+<span class="error"><b><?php echo $login_failed; ?></b></span>
 
-<div>
-<div>Username Or Email: </div>
+<div class="inputtextbox">
+<div class="inputtext">Username Or Email: </div>
 <div>
 <input type="text" name="username" value="<?php echo set_value('username'); ?>"/>
 
 </div>
 </div>
 
+<div class="inputtextbox">
+<div class="inputtext">Password: </div>
 <div>
-<div>Password: </div>
-<div>
-
+<div >
 <input type="password" name="password" value="<?php echo set_value('password'); ?>" /><br/>
 </div>
 </div>
+</div>
 
 <div>
-<input type="submit" value="Submit" name="submit_login"/>
+<input type="submit" class="orangebutton orangebuttonhover" value="Submit" name="submit_login"/>
 </div>
 
 <?php echo form_close()?>
--- csimcw/application/views/_output_html.php
+++ csimcw/application/views/_output_html.php
@@ -2,13 +2,18 @@
 <html>
 <head>
 <title> CodeIgniter Login and Registration System With Captcha </title>
-<link href="<?php echo base_url().'css/style.css'; ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url('css/styless.css'); ?>" rel="stylesheet" type="text/css" />
+
 </head>
 <body>
+<div class="wrapper">
+	<div class="link">
 	<?php echo $menu_top; ?>
+	</div>
 	<br/>
 	<div>
 		<?php echo $body; ?>
 	</div>
+</div>
 </body>
 </html>
